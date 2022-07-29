2b8bb1dccfac0bfbfa5e6e53c51144215bb7d259
macpat83 <macpat@gmail.com>
2022-07-28 19:25:15 -0500

    feature/user-routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const { appendFile } = require('fs');
+// Require express and mongoose
+const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
+app.use(require('./routes'));
 // Connect mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
   useFindAndModify: false,
